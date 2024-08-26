a8c3c385b69d52875dc690732a115217ff7d4d6f
jin <jin337x@outlook.com>
2024-08-26 13:19:40 +0800

    修改跳转

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Outlet, useLocation } from 'react-router-dom'
 
 const menu = [
@@ -14,22 +15,28 @@ const menu = [
 
 const Home = () => {
   const location = useLocation()
+  const navigate = useNavigate()
   const [active, setActive] = useState()
 
   useEffect(() => {
     setActive(location.pathname)
   }, [location])
 
+  const selectHandler = (link) => {
+    setActive(link)
+    navigate(link)
+  }
+
   return (
     <div className='box-border flex h-screen p-4'>
       <div className='flex h-full w-[200px] flex-col py-7'>
         {menu.map((item) => (
-          <a
+          <div
             key={item.name}
-            href={item.link}
+            onClick={() => selectHandler(item.link)}
             className={`mb-1 rounded-s-lg px-4 py-2.5 hover:bg-white ${item.link === active ? 'bg-white' : ''}`}>
             {item.name}
-          </a>
+          </div>
         ))}
       </div>
       <div className='content w-[calc(100%-200px)] overflow-y-auto bg-white p-3'>
